73a2b93696c5c1ab2ef4a0496d62717ad223b1b8
sapafe <sarpalfer@alu.edu.gva.es>
2026-03-03 17:34:30 +0100

    a vore si s'arregla

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::view('/', 'home');
-Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
-Route::get('/blog/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::get('/blog', [PostController::class , 'index'])->name('posts.index');
+Route::get('/blog/{post}', [PostController::class , 'show'])->name('posts.show');
 
 Route::middleware('auth')->group(function () {
-    Route::post('/la-casella/reservar', [ReservationController::class, 'store'])->name('casella.store');
+    Route::get('/la-casella', [ReservationController::class , 'create'])->name('casella.create');
+    Route::post('/la-casella/reservar', [ReservationController::class , 'store'])->name('casella.store');
 });
-Route::get('/login', [AuthController::class, 'show'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/login', [AuthController::class , 'show'])->name('login');
+Route::post('/login', [AuthController::class , 'login'])->name('login.attempt');
+Route::post('/logout', [AuthController::class , 'logout'])->name('logout');
